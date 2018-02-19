003be262f2e5c994e0cd8e08be41cbdd40aca1c2
Khabenko <m.habenko@ukr.net>
2018-02-19 22:38:26 +0200

    Initial commit

---
+++ android/src/State/MenuState.java
@@ -0,0 +1,48 @@
+package State;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Maka on 17.02.2018.
+ */
+
+public class MenuState extends State.State {
+    private Texture backgroundMenu;
+    private State.State state;
+
+
+    public MenuState(StateManager sm) {
+    super(sm);
+    this.backgroundMenu = new Texture("Background\\Menu.jpg");
+
+
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        sb.begin();
+        sb.draw(backgroundMenu, 0, 0, camera.viewportWidth, camera.viewportHeight);
+        sb.end();
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ android/src/State/State.java
@@ -0,0 +1,33 @@
+package State;
+
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+
+/**
+ * Created by Maka on 17.10.2017.
+ */
+
+public abstract class State {
+   protected OrthographicCamera camera;
+   protected Vector3 mouse;
+   protected StateManager sm;
+   protected float oneX;
+   protected float oneY;
+
+    State(StateManager sm) {
+        this.camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        this.mouse = new Vector3();
+        this.sm = sm;
+        this.oneX = camera.viewportWidth/100;
+        this.oneY = camera.viewportWidth/100;
+    }
+
+    protected abstract void handleInput();
+    public abstract void update(float dt);
+    public abstract void render(SpriteBatch sb);
+    public abstract void dispose();
+}
